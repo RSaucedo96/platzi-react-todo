4b48859ebd7e1d8afbd0f6dc2d433f6c348db5e0
Ricardo <ricardoarielsaucedo96@gmail.com>
2022-12-01 09:03:10 -0300

    reformated error,loading and empty todos to their own components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { useTodos } from "./useTodos";
-import { TodoCounter } from './components/TodoCounter';
-import { TodoItem } from './components/TodoItem';
-import { TodoList } from './components/TodoList';
-import { TodoSearch } from './components/TodoSearch';
-import { CreateTodoButton } from './components/CreateTodoButton';
-import { Modal } from "./components/Modal";
-import { TodoForm } from "./components/TodoForm";
+import {useTodos} from "./useTodos";
+import {TodoCounter} from './components/TodoCounter';
+import {TodoItem} from './components/TodoItem';
+import {TodoList} from './components/TodoList';
+import {TodoSearch} from './components/TodoSearch';
+import {TodosLoading} from "./components/TodosLoading";
+import {TodosError} from "./components/TodosError";
+import {EmptyTodos} from "./components/EmptyTodos";
+import {CreateTodoButton} from './components/CreateTodoButton';
+import {Modal} from "./components/Modal";
+import {TodoForm} from "./components/TodoForm";
 
 function App() {
     const {
@@ -49,11 +52,19 @@ function App() {
                     totalTodos={totalTodos}
                     completedTodos={completedTodos}
                 />
-                <TodoList>
-                    {error && <p>Hubo un error</p>}
-                    {loading && <p>cargando</p>}
-                    {(!loading && !searchedTodos.length) && <p>crea tu primer todo</p>}
-                    {searchedTodos.map(todo => (
+                <TodoList
+                    loading={loading}
+                    error={error}
+                    searchedTodos={searchedTodos}
+                    searchText={searchValue}
+                    totalTodos={totalTodos}
+                    onError={() => <TodosError/>}
+                    onLoading={() => <TodosLoading/>}
+                    onEmptyTodos={() => <EmptyTodos/>}
+                    onEmptySearchResults={
+                        (searchText) => <p>no hay resultados para {searchText}</p>
+                    }
+                    {todo => (
                         <TodoItem
                             key={todo.text}
                             text={todo.text}
@@ -61,8 +72,8 @@ function App() {
                             onComplete={() => completeTodo(todo.text)}
                             onDelete={() => deleteTodo(todo.text)}
                         />
-                    ))}
-                </TodoList>
+                    )}
+                />
             </div>
         </React.Fragment>
     );
